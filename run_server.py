43199d720c76f7db25acac39a4bdd41d3aa0b220
ibuler <ibuler@qq.com>
2017-03-26 11:49:28 +0800

    Update api

--- run_server.py
+++ run_server.py
@@ -5,6 +5,12 @@
 from __future__ import absolute_import
 import os
 
+try:
+    os.mkdir('logs')
+    os.mkdir('keys')
+except:
+    pass
+
 from coco.app import Coco
 from coco.tasks import command_task, record_task
 
@@ -12,11 +18,6 @@ from coco.tasks import command_task, record_task
 coco = Coco()
 
 if __name__ == '__main__':
-    try:
-        os.mkdir('logs')
-        os.mkdir('keys')
-    except:
-        pass
     command_task.run()
     record_task.run()
     coco.run_forever()
