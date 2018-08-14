edc57dc7654ee7c0b1f0419dab2744f29b6729c3
ibuler <ibuler@qq.com>
2018-08-14 13:02:38 +0800

    [Update] 修改脚本

--- utils/makemessages.sh
+++
@@ -1,7 +0,0 @@
-# makemassages -> .pot .po
-# 
-
-xgettext -k_ -o pot/coco.pot --from-code=UTF-8 coco/*.py
-msginit -l locale/zh_CN/LC_MESSAGES/coco -i pot/coco.pot
-msginit -l locale/en/LC_MESSAGES/coco -i pot/coco.pot
-
