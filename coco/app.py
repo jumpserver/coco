d7ba5b58d2c7b9e7fa3ea61f2f3f96aed740e2c4
ibuler <ibuler@qq.com>
2018-10-12 15:27:11 +0800

    [Update] 修改版本号

--- coco/app.py
+++ coco/app.py
@@ -21,7 +21,7 @@ from .session import Session
 from .models import Connection
 
 
-__version__ = '1.4.2'
+__version__ = '1.4.3'
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 logger = get_logger(__file__)
