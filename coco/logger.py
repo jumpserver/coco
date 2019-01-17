87dea073605e0e75f46978a29a91037dac585da8
ibuler <ibuler@qq.com>
2019-01-17 17:38:42 +0800

    [Update] 修改log文件名字

--- coco/logger.py
+++ coco/logger.py
@@ -3,6 +3,7 @@
 #
 
 import os
+import socket
 import logging
 from logging.config import dictConfig
 from .conf import config as app_config
@@ -11,9 +12,10 @@ from .conf import config as app_config
 def create_logger():
     level = app_config['LOG_LEVEL']
     log_dir = app_config['LOG_DIR']
+    filename = '{}.log'.format(socket.gethostname())
     if not os.path.isdir(log_dir):
         os.makedirs(log_dir)
-    log_path = os.path.join(log_dir, 'coco.log')
+    log_path = os.path.join(log_dir, filename)
     main_setting = {
         'handlers': ['console', 'file'],
         'level': level,
